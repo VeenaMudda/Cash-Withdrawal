2eeb5d8b3ee2d8f94e33884be6cf39477d1319d8
Veena Mudda <110976068+VeenaMudda@users.noreply.github.com>
2022-10-06 15:38:17 +0530

    Create index.js

---
+++ src/components/DenominationItem/index.js
@@ -0,0 +1,16 @@
+// Write your code here
+import './index.css'
+
+const DenominationItem = props => {
+  const {withdraw, amount} = props
+  const onWithdraw = () => {
+    withdraw()
+  }
+  return (
+    <button type="button" className="button" onClick={onWithdraw}>
+      {amount}
+    </button>
+  )
+}
+
+export default DenominationItem
